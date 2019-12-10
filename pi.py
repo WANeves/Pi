42967d21781559954e69ceb814baf87afe862b05
Wellington Alves das Neves <wellingtonalvesneves@gmail.com>
2019-12-10 17:27:05 -0300

    position line

--- pi.py
+++ pi.py
@@ -42,14 +42,17 @@ if __name__ == "__main__":
         y = random() * 2 - 1
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
-
+    
     factory = ChromosomeFactory.ChromosomeRangeFactory(minValue=1,
                         maxValue=100,noOfGenes=8,duplicates=False)
     factory.createChromosome()
     
     print("Chromosome")
+    
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+
+    
     
     spark.stop()
     
